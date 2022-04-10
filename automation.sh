b614de38d2ae191c6ebd2428f2f777393b5df909
Esteban <esteban.minici@gmail.com>
2022-04-10 11:35:15 -0700

    ADD:Finalizado el script y testeado

--- automation.sh
+++ automation.sh
@@ -5,5 +5,22 @@ mkdir tempdir
 cp -r static tempdir
 cp -r templates tempdir
 cp desafio2_app.py tempdir
+#Me ubico en tempdir para generar el dockerfile
+cd tempdir
+#Genero dockerfile
+touch dockerfile
+#Agrego el contenido al dockerfile
+echo "FROM python
+RUN pip install flask
+COPY ./static /home/myapp/static/
+COPY ./templates /home/myapp/templates/
+COPY desafio2_app.py /home/myapp/
+EXPOSE 5050
+CMD python3 /home/myapp/desafio2_app.py" > dockerfile
+#Genero el contenedor desde el dockerfile generado anteriormente
+sudo docker build -t appdesafio2 .
+#Ejecuto el contenedor 
+sudo docker run -t -d -p 5050:5050 --name appdesafio2running appdesafio2
+#Consulto si el contenedor se esta ejecutando
+sudo docker ps
 
-echo "hello world"
