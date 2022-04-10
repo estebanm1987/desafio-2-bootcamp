6fc88a21f291ecc43aa007a6c6c7c041850bc0e5
Esteban <esteban.minici@gmail.com>
2022-04-10 10:49:57 -0700

    ADD:Se agrega automatizacion de generacion de directorio y copia de contenido

--- automation.sh
+++ automation.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+#Creo carpeta tempdir
+mkdir tempdir
+#Copio contenido de static y templates dentro de tempdir, tambien el archivo de python
+cp -r static tempdir
+cp -r templates tempdir
+cp desafio2_app.py tempdir
 
 echo "hello world"
